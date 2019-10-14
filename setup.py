d70be75bf18a523a2d36dbb5c4755998ca951d02
Luke Arend <luke@boonlogic.com>
2019-10-14 11:00:48 -0500

    package name boonnanopyapi

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 
 from setuptools import setup, find_packages
 
-setup(name='myproject', version='1.0', packages=find_packages())
+setup(name='boonnanopyapi', version='1.0', packages=find_packages())
