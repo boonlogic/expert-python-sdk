81fa6cfb4ab61bb7159144d1096561d8a54d905c
Jim Turnquist <jim@boonlogic.com>
2020-01-13 11:09:13 -0600

    Update version to 3.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='boonnano',
-    version='3.0.6',
+    version='3.0.7',
     author="BoonLogic",
     author_email="elise@boonlogic.com",
     packages=['boonnano'],
